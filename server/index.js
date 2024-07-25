1948724606e04979891f348d47e809bba9d3151d
Sudipta Mondal <msudipta857@gmail.com>
2024-07-25 12:01:36 +0530

    add env in server

--- server/index.js
+++ server/index.js
@@ -1,31 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+ require('dotenv').config();
 const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect("mongodb://localhost:27017/TestforApp", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGOOSE_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-  })
+    app.listen(process.env.PORT, () => {
+      console.log("Server is running on port 5000");     
+    });
+  }) 
   .catch((err) => {
     console.error("Failed to connect to MongoDB", err);
   });
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
-  next();
-});
+  next(); 
+}); 
 
 app.use("/api/users", require("./routes/authRoute"));
 app.use("/api", require("./routes/todoRoutes"));
 app.use('/uploads',express.static('uploads'))
 app.use('/profile',require('./routes/imageRoute'));
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
-});
+
